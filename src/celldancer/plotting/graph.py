b05cab42554dc73997b3eda344aa5ec679b26ddf
Pengzhi Zhang <zhangpengzhi1988@gmail.com>
2022-06-21 16:40:40 -0500

    PTO graph
    q
    quit
    exit

--- src/celldancer/plotting/graph.py
+++ src/celldancer/plotting/graph.py
@@ -31,11 +31,11 @@ def PTO_Graph(
 
     """ 
     Graph visualization of selected cells reflecting their orders in
-    pseudotime. Embedding and pseudotime of the cells are required. Each cell
-    makes a node and the connection between nodes are based on their separation 
-    in the embedding space and the strength of the connection is propotional to 
-    the pseudotime difference (the larger the pseudotime difference in absolute 
-    values, the weaker the connection).
+    pseudotime (PseudoTimeOrdered_Graph: PTO_Graph). Embedding and pseudotime 
+    of the cells are required. Each cell makes a node and the connection between 
+    nodes are based on their separation in the embedding space and the strength 
+    of the connection is propotional to the pseudotime difference (the larger 
+    the pseudotime difference in absolute values, the weaker the connection).
 
     Example usage:
 
