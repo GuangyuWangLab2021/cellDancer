8f80ffe9c428a022a2c92d76263fd44a4cbd5159
Pengzhi Zhang <pzhang8@uh.edu>
2022-08-08 23:20:34 -0500

    fixing inconsistent reference to the README file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("readme.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
