8b65f820ab91099f8a9cd86e3c6c75ec8b64353d
ShengyuLi <oceanlisy@gmail.com>
2023-03-21 16:49:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,25 @@ setuptools.setup(
     package_data={'': ['model/*.pt']},
     include_package_data=True,
     python_requires=">=3.7.6",
+    install_requires = ['pytorch-lightning==1.5.2',
+                        'torch==1.10.0',
+                        'pandas==1.3.4',
+                        'numpy==1.20.3',
+                        'anndata==0.8.0',
+                        'tqdm==4.62.3',
+                        'scikit-learn==1.0.1',
+                        'scipy==1.7.2',
+                        'joblib==1.1.0',
+                        'scikit-image==0.19.2',
+                        'statsmodels==0.13.1',
+                        'matplotlib==3.5.3',
+                        'seaborn==0.11.2',
+                        'datashader==0.14.0',
+                        'bezier==2021.2.12',
+                        'umap-learn==0.5.2',
+                        'jupyterlab==3.3.4',
+                        'setuptools==59.5.0',
+                        'setuptools-scm==6.3.2'
+                        ]
 )
 
