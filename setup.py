c5d5727dd87fb8d02d1422d11d07dfa825cbff5e
ShengyuLi <oceanlisy@gmail.com>
2023-03-21 16:43:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import setuptools
 
-with open("readme.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="celldancer",
     version="1.1.3",
     author="Wang Lab",
     author_email="gwang2@houstonmethodist.org",
     description="Study RNA velocity through neural network.",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
